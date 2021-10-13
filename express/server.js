8b91caa8d5065a2839c1139aeb45cae1ce4c763e
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 16:46:36 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -28,7 +28,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.write('OK');
 
 module.exports = app;
 module.exports.handler = serverless(app);
